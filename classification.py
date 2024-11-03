b03a55724335f7911c4766ab8913acb792b79dcc
Amirul Erfan <93848257+amirulerfn@users.noreply.github.com>
2024-11-03 13:15:16 +0800

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import scikit_learn
+
 df = pd.read_csv('https://raw.githubusercontent.com/amirulerfn/jie43202/refs/heads/main/Toddler%20Autism%20dataset%20July%202018.csv')
 st.write(df)
 # Detecting any missing values
