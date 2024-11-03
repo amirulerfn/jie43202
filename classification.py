0a25bb43a199a24c8bad54edca8c646b3e5019a6
Amirul Erfan <93848257+amirulerfn@users.noreply.github.com>
2024-11-03 12:48:23 +0800

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,1 +1,4 @@
-
+import pandas as pd
+import numpy as np
+df = pd.read_csv('https://raw.githubusercontent.com/tirasyaz/autism_prediction/refs/heads/main/Toddler%20Autism%20dataset%20July%202018.csv')
+st.write(df)
