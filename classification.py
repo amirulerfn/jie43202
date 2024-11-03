62c519abbd320ca925f295d07c2978eac4a53e33
Amirul Erfan <93848257+amirulerfn@users.noreply.github.com>
2024-11-03 12:55:36 +0800

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-df = pd.read_csv('https://raw.githubusercontent.com/tirasyaz/autism_prediction/refs/heads/main/Toddler%20Autism%20dataset%20July%202018.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/amirulerfn/jie43202/refs/heads/main/Toddler%20Autism%20dataset%20July%202018.csv')
 st.write(df)
